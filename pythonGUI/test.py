50eaeae69d02ce206847f9e5b01295f579de99e8
HarenBaren <95982865+HarenBaren@users.noreply.github.com>
2023-09-29 16:13:03 -0400

    Setup this shit

--- pythonGUI/test.py
+++ pythonGUI/test.py
@@ -1,13 +1,42 @@
 from tkinter import *
 
-bwindow = Tk()
+# define and basic window info
+frame = Tk()
+frame.title("weird window")
+frame.geometry("350x200")
 
-bwindow.title("Simple calc")
+# text boxes
+lbl = Label(frame, text="text1")
+lbl.grid(row=0, column=0)
 
-bwindow.geometry("840x600")
+lbl2 = Label(frame, text="text2")
+lbl2.grid(row=1, column=0)
 
-lbl = Label(bwindow, text = "Calc")
-lbl.grid()
+lbl3 = Label(frame, text="Click ze button")
+lbl3.grid(row=3, column=1)
 
-bwindow.mainloop()
+# input
+entry1 = Entry(frame)
+entry1.grid(row=0, column=1)
 
+entry2 = Entry(frame)
+entry2.grid(row=1, column=1)
+
+
+# function for button
+def clicked():
+    x = entry1.get()
+    y = entry2.get()
+
+    if x == y:
+        lbl3.configure(text="True")
+    else:
+        lbl3.configure(text="Not True")
+
+
+# buttons
+button = Button(frame, text="Validate", command=clicked())
+button.grid(row=2, column=1)
+
+# runs program
+frame.mainloop()
