90aa233a8f203742ba03fab72908c67a9f471db7
HarenBaren <95982865+HarenBaren@users.noreply.github.com>
2023-10-06 11:46:59 -0400

    Starting over

--- pythonGUI/test.py
+++ pythonGUI/test.py
@@ -1,42 +1,9 @@
-from tkinter import *
+import customtkinter
 
-# define and basic window info
-frame = Tk()
-frame.title("weird window")
-frame.geometry("350x200")
+#main frame details
+frame = customtkinter.CTk()
+frame.geometry("600x500")
+frame.title("This Works")
 
-# text boxes
-lbl = Label(frame, text="text1")
-lbl.grid(row=0, column=0)
-
-lbl2 = Label(frame, text="text2")
-lbl2.grid(row=1, column=0)
-
-lbl3 = Label(frame, text="Click ze button")
-lbl3.grid(row=3, column=1)
-
-# input
-entry1 = Entry(frame)
-entry1.grid(row=0, column=1)
-
-entry2 = Entry(frame)
-entry2.grid(row=1, column=1)
-
-
-# function for button
-def clicked():
-    x = entry1.get()
-    y = entry2.get()
-
-    if x == y:
-        lbl3.configure(text="True")
-    else:
-        lbl3.configure(text="Not True")
-
-
-# buttons
-button = Button(frame, text="Validate", command=clicked())
-button.grid(row=2, column=1)
-
-# runs program
+#main program loop
 frame.mainloop()
