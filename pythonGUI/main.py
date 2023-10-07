d9bd74d25c2c3b512c4e347b67cbfb1eacdef02a
HarenBaren <95982865+HarenBaren@users.noreply.github.com>
2023-10-06 23:32:37 -0400

    Starting to switch from grid() to pack()

--- pythonGUI/main.py
+++ pythonGUI/main.py
@@ -1,33 +1,47 @@
 import customtkinter
+
 customtkinter.set_appearance_mode("System")
+
+
 # def functions
 def mathsolver():
+    print()
 
 
 # main frame details
-frame = customtkinter.CTk()
-frame.geometry("400x300")
-frame.title("This Works")
+
+
+root = customtkinter.CTk()
+root.maxsize(400,300)
+root.title("This Works")
+
+
+# base frames
+frame_1 = customtkinter.CTkFrame(frame,)
 
 # user input
 entry = customtkinter.CTkEntry(frame, placeholder_text="Enter a value", justify="center")
-entry.grid(anchor="NE")
+entry.pack(padx=20, pady=20)
+
+# result
+result = customtkinter.CTkEntry(frame, placeholder_text="Result", justify="center")
+result.pack(fill='x')
 
 # buttons
-plus = customtkinter.CTkButton(frame, text="+") #command=buttonclick)
-plus.grid(row=1, column=0, padx=10, pady=10)
+plus = customtkinter.CTkButton(frame, text="+")  # command=buttonclick)
+plus.pack(padx=10, pady=10)
 
-minus = customtkinter.CTkButton(frame, text="-") #command=buttonclick)
-minus.grid(row=1, column=1, padx=10, pady=10)
+minus = customtkinter.CTkButton(frame, text="-")  # command=buttonclick)
+minus.pack(padx=10, pady=10)
 
-mul = customtkinter.CTkButton(frame, text="*") #command=buttonclick)
-mul.grid(row=2, column=0, padx=10, pady=10)
+mul = customtkinter.CTkButton(frame, text="*")  # command=buttonclick)
+mul.pack(padx=10, pady=10)
 
-div = customtkinter.CTkButton(frame, text="/") #command=buttonclick)
-div.grid(row=2, column=1, padx=10, pady=10)
+div = customtkinter.CTkButton(frame, text="/")  # command=buttonclick)
+div.pack(padx=10, pady=10)
 
-equal = customtkinter.CTkButton(frame, text="=") #command=buttonclick)
-equal.grid(row=3, column=0, padx=10, pady=10)
+equal = customtkinter.CTkButton(frame, text="=")  # command=buttonclick)
+equal.pack(padx=10, pady=10)
 
 # main program loop
 frame.mainloop()
--- pythonGUI/test.py
+++
@@ -1,9 +0,0 @@
-import customtkinter
-
-#main frame details
-frame = customtkinter.CTk()
-frame.geometry("600x500")
-frame.title("This Works")
-
-#main program loop
-frame.mainloop()
