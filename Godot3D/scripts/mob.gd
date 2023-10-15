82a227d8e972e54696fb730814b80383916637a2
HarenBaren <95982865+HarenBaren@users.noreply.github.com>
2023-10-14 21:45:02 -0400

    doin it again

--- Godot3D/scripts/mob.gd
+++ Godot3D/scripts/mob.gd
@@ -4,18 +4,18 @@ extends CharacterBody3D
 
 @export var max_speed = 18
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	move_and_slide()
 	
 func initialize(start_position, player_position):
 	
 	look_at_from_position(start_position, player_position, Vector3.UP)
 	
-	rotate_y(randf_range(-PI / 4, PI / 4)
+	rotate_y(randf_range(-PI / 4, PI / 4))
 	
 	var random_speed = randi_range(min_speed, max_speed)
 	
-	velocity = Vector3.FOWARD * random_speed
+	velocity = Vector3.FORWARD * random_speed
 	
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
