90d5840387e2da982748ab055fbd648fadc0fa78
HarenBaren <95982865+HarenBaren@users.noreply.github.com>
2023-10-14 21:43:28 -0400

    Hm?

--- Godot3D/scripts/mob.gd
+++ Godot3D/scripts/mob.gd
@@ -11,11 +11,11 @@ func initialize(start_position, player_position):
 	
 	look_at_from_position(start_position, player_position, Vector3.UP)
 	
-	rotate_y(randf_range(-PI / 4, PI / 4)
+	rotate_y(randf_range(-PI / 4, PI / 4))
 	
 	var random_speed = randi_range(min_speed, max_speed)
 	
-	velocity = Vector3.FOWARD * random_speed
+	velocity = Vector3.FORWARD * random_speed
 	
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
