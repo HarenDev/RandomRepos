7f7c2d972f589e0eb9cd74b6e56a53a8631a5dd0
HarenBaren <95982865+HarenBaren@users.noreply.github.com>
2023-11-02 15:13:59 -0400

    Did alot with Godot; Started working on another calculator

---
+++ pythonGUI/main.py
@@ -0,0 +1,15 @@
+import customtkinter
+import math
+
+#Functions
+
+#Window Logic
+root = customtkinter.CTk()
+
+
+
+
+root.title("Calc")
+root.geometry("400x400")
+root.resizable(False,False)
+root.mainloop()
