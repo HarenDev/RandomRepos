4f01e029c3f2250b4dc41e530de693f38d7af053
HarenBaren <95982865+HarenBaren@users.noreply.github.com>
2023-11-03 07:45:25 -0400

    Making the basic windowing for the calc

--- pythonGUI/main.py
+++ pythonGUI/main.py
@@ -1,15 +1,22 @@
 import customtkinter
 import math
 
-#Functions
-
-#Window Logic
 root = customtkinter.CTk()
 
+#Functions
 
+#Frames Logic
+button_plus = customtkinter.CTkButton(root, text="+")
+button_plus.grid(row=0,column=0,padx=20,pady=20,sticky="ew",columnspan=2)
+button_sub = customtkinter.CTkButton(root, text="-")
+button_sub.grid(row=1,column=0,padx=20,pady=20,sticky="ew",columnspan=2)
+button_mul = customtkinter.CTkButton(root, text="*")
+button_mul.grid(row=2,column=0,padx=20,pady=20,sticky="ew",columnspan=2)
+button_div = customtkinter.CTkButton(root, text="/")
+button_div.grid(row=3,column=0,padx=20,pady=20,sticky="ew",columnspan=2)
 
-
+#Window Logic
 root.title("Calc")
-root.geometry("400x400")
+root.geometry("200-40x400")
 root.resizable(False,False)
 root.mainloop()
